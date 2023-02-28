0e73196b4388e45c9f6fb1a7a319a2fe16498873
Class Account <ee106b-aas@c105-4.local>
2023-02-28 12:33:23 -0800

    setup proj2

---
+++ proj2/build/stdr_simulator/stdr_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106b/sp23/class/ee106b-aas/ros_workspaces/proj2/src/stdr_simulator/stdr_robot
+# Build directory: /home/cc/ee106b/sp23/class/ee106b-aas/ros_workspaces/proj2/build/stdr_simulator/stdr_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
